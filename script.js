aa109d7ec2fca889297c4406b2e44956746983a2
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-03-24 13:46:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ function prom()
+	 {
+		 return new Promise(function(resolve){
+	 setTimeout(()=>{
+	 
+
+		 resolve("Hello World");
+	
+	 
+	 },1000)
+		 })
+	 }
+prom().then(function (p) {
+	const output =document.getElementById("output");
+	output.innerText=p;
+})
+	
+
+	
